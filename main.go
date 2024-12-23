5b464e3fe733d1d60c74b75948164a913603b583
ArrafiM <arrafimaulana12@gmail.com>
2024-12-23 13:35:47 +0700

    update: index api route

--- main.go
+++ main.go
@@ -18,12 +18,17 @@ func main() {
 
 	router.Static("/public", "./public")
 
-	router.GET("/", index)
+	router.GET("/api", index)
 
 	routes.IndexRoutes(router)
 
 	//websocket
 	// routes.SocketRoute(router)
+	// router.Run("192.168.1.2:8080")
+	// router.Run("192.168.18.32:8080")
+	// router.Run("192.168.19.108:8080")
+	// router.Run("172.20.10.3:8080")
+	// router.Run("192.168.19.14:8080")
 	router.Run("localhost:8080")
 
 }
